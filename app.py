ee1214711d4a730daa89963a0845729b9fa6acc6
Prathamesh <prathameshw95@gmail.com>
2023-08-18 23:13:15 +0530

    specified the host and port in flask

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run()
+    app.run(host = "0.0.0.0", port = 5000)
