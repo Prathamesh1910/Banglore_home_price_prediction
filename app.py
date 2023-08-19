b057df76af42b4821dc5e6bd12b8d78812ea74ca
Prathamesh1910 <64536495+Prathamesh1910@users.noreply.github.com>
2023-08-20 00:18:51 +0530

    Update app.py
    changed the port in app.run()

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(host = "0.0.0.0", port = 5000)
+    app.run(host = "0.0.0.0", port = 8080)
