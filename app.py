2f59c26981cd1fbf4f985c0b018b05c2a675df31
Prathamesh1910 <64536495+Prathamesh1910@users.noreply.github.com>
2023-08-24 21:00:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from models import util
 
 app = Flask(__name__)
+CORS(app, origins='*')
 
 @app.route('/')
 def home():
