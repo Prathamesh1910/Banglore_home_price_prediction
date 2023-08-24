3854256fe8633891f0c520aad6f8c7993f36f3d7
Prathamesh1910 <64536495+Prathamesh1910@users.noreply.github.com>
2023-08-24 20:18:28 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pull the latest Docker image
+docker pull $AWS_ECR_LOGIN_URI/$ECR_REPOSITORY_NAME:latest
+
+# Check if container "mltest1" is running and stop/remove it
+if docker ps -q --filter "name=mltest1" | grep -q .; then
+    docker stop mltest1
+    docker rm -fv mltest1
+fi
+
+# Run Docker image to serve users
+docker run -d -p 8080:8080 --ipc="host" --name=mltest1 \
+    -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+    -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+    -e "AWS_REGION=$AWS_REGI
+    
