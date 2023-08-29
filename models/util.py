f3b290eb0cc182afefc611c7988b4ed500084498
Prathamesh <prathameshw95@gmail.com>
2023-08-29 17:16:50 +0530

    updated util file

--- models/util.py
+++ models/util.py
@@ -9,7 +9,7 @@ __data_columns = None
 __least_locations = None
 __locations = None
 __area_type = None
-__availability = None
+
 path: Path = Path(__file__).parent.parent
 
 
@@ -19,17 +19,16 @@ def load_saved_artifacts():
     global __least_locations
     global __locations
     global __area_type
-    global __availability
 
     logging.info('Loading saved artifacts')
 
     with open(path.joinpath("models/artifacts/columns.json"), 'r') as f:
         __data_columns = json.load(f)['data_columns']
 
-    with open(path.joinpath("models/artifacts/bhp_model_en.pickle"), 'rb') as f:
+    with open(path.joinpath("models/artifacts/bhp_model_dtr.pickle"), 'rb') as f:
         __model = pickle.load(f)
 
-    with open(path.joinpath("models/artifacts/least_locations1.json"), 'r') as f:
+    with open(path.joinpath("models/artifacts/least_locations.json"), 'r') as f:
         __least_locations = json.load(f)['least_locations']
 
     with open(path.joinpath("models/artifacts/locations.json"), 'r') as f:
@@ -37,9 +36,6 @@ def load_saved_artifacts():
 
     with open(path.joinpath("models/artifacts/area_type.json"), 'r') as f:
         __area_type = json.load(f)["area_types"]
-
-    with open(path.joinpath("models/artifacts/availability.json"), 'r') as f:
-        __availability = json.load(f)['month_availability']
 
     logging.info('Artifacts loaded')
 
@@ -52,16 +48,13 @@ def get_area_type():
     return __area_type
 
 
-def get_availability():
-    return __availability
-
 
 def scale_params(size: int, total_sqft: float, bath: int, balcony: int) -> list:
     scaled_params = []
 
-    min_size, max_size = 0.5, 9
-    min_area, max_area = 296.0, 42000.0
-    min_bath, max_bath = 1, 9
+    min_size, max_size = 1, 9
+    min_area, max_area = 300.0, 42000.0
+    min_bath, max_bath = 1, 10
     min_balc, max_balc = 0, 3
 
     scaled_params.append(round((size - min_size) / (max_size - min_size), 4))
@@ -74,15 +67,23 @@ def scale_params(size: int, total_sqft: float, bath: int, balcony: int) -> list:
 
 def predict_price(**kwargs) -> float:
 
-    size, total_sqft, bath, balcony, area_type, location, month_availability = kwargs['size'], kwargs['total_sqft'], kwargs['bath'], kwargs['balcony'], \
-                                                                               kwargs['area_type'], kwargs['location'], kwargs['month_availability']
+    size, total_sqft, bath, balcony, area_type, location = kwargs['size'], kwargs['total_sqft'], kwargs['bath'], kwargs['balcony'], \
+                                                                               kwargs['area_type'], kwargs['location']
 
     if location in __least_locations:
         location = 'Other'
 
+    if 'JP' in location.split():
+        location = 'JP Nagar'
+    elif 'Jayanagar' in location.split():
+        location = 'Jayanagar'
+    elif 'Banashankari' in location.split():
+        location = 'Banashankari'
+
+    print(location)
     x = np.zeros(len(__data_columns))
 
-    index1, index2, index3 = 0, 0, 0
+    index1, index2 = 0, 0
 
     try:
         index1 = __data_columns.index(area_type.lower())
@@ -94,10 +95,6 @@ def predict_price(**kwargs) -> float:
     except:
         None
 
-    try:
-        index3 = __data_columns.index(month_availability.lower())
-    except:
-        None
 
     scaled_params = scale_params(size, total_sqft, bath, balcony)
 
@@ -110,12 +107,10 @@ def predict_price(**kwargs) -> float:
         x[index1] = 1
     if index2 > 0:
         x[index2] = 1
-    if index3 > 0:
-        x[index3] = 1
 
-    return round(__model.predict([x])[0] * total_sqft / 100000, 2)
+    return round(__model.predict([x])[0], 2)
 
 
 if __name__ == '__main__':
     load_saved_artifacts()
-    print(predict_price(**{'size': 4, 'total_sqft': 1500.0, 'bath': 3, 'balcony': 4, 'area_type': 'Carpet Area', 'location': 'JP Nagar', 'month_availability': 'Jun'}))
+    print(predict_price(**{'size': 3, 'total_sqft': 1000.0, 'bath': 5, 'balcony': 3, 'area_type': 'Plot Area', 'location': 'Volagerekallahalli'}))
