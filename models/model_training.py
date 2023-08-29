7f29b46b90ea927121fd2fa17c0c78b438e26242
Prathamesh <prathameshw95@gmail.com>
2023-08-29 17:16:14 +0530

    updated the mlflow and new prediction model

--- models/model_training.py
+++ models/model_training.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 import pickle
@@ -26,7 +27,7 @@ if __name__ == '__main__':
     warnings.filterwarnings("ignore")
 
     try:
-        df = pd.read_csv('../data/bhp_data.csv')
+        df = pd.read_csv('../data/bhp_data_updated.csv')
     except Exception as e:
         logger.exception(
             "Unable to download training & test CSV, check your internet connection. Error: %s", e
@@ -34,31 +35,47 @@ if __name__ == '__main__':
 
     df.drop(columns=['Unnamed: 0'], inplace = True)
 
-    inputs = df.drop('price_per_sqft', axis = 'columns')
-    target = df['price_per_sqft']
+    inputs = df.drop('price', axis = 1)
+    target = df['price']
 
     x_train, x_test, y_train, y_test = train_test_split(inputs.values, target, test_size = 0.2)
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.0001
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.01
+    model_dtr = DecisionTreeRegressor(max_depth = 6, min_samples_leaf = 2, min_samples_split = 3)
+
+    model_dtr.fit(x_train, y_train)
+
+    y_pred1 = model_dtr.predict(x_train)
+    y_pred2 = model_dtr.predict(x_test)
+
+    (rmse_train, mae_train, r2_train) = eval_metrics(y_train, y_pred1)
+    (rmse_test, mae_test, r2_test) = eval_metrics(y_test, y_pred2)
+
+    print(rmse_train, rmse_test)
+    print(mae_train, mae_test)
+    print(r2_train, r2_test)
+
+    max_depth = int(sys.argv[1]) if len(sys.argv) > 1 else 6
+    min_samples_leaf = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+    min_samples_split = int(sys.argv[3]) if len(sys.argv) > 3 else 3
 
 
     with mlflow.start_run():
-        model_en = ElasticNet(alpha = alpha, l1_ratio = l1_ratio)
-        model_en.fit(x_train, y_train)
+        model_dtr = DecisionTreeRegressor(max_depth = max_depth, min_samples_leaf = min_samples_leaf, min_samples_split = min_samples_split)
+        model_dtr.fit(x_train, y_train)
 
-        y_pred1 = model_en.predict(x_train)
-        y_pred2 = model_en.predict(x_test)
+        y_pred1 = model_dtr.predict(x_train)
+        y_pred2 = model_dtr.predict(x_test)
 
         (rmse_train, mae_train, r2_train) = eval_metrics(y_train, y_pred1)
         (rmse_test, mae_test, r2_test) = eval_metrics(y_test, y_pred2)
 
-        print(f"with alpha = {alpha} and l1_ratio = {l1_ratio}")
+        print(f"with max_depth = {max_depth}, min_samples_leaf = {min_samples_leaf} and min_samples_split = {min_samples_split}")
         print(f"rmse_train = {rmse_train}, mae_train = {mae_train}, r2_train = {r2_train}")
         print(f"rmse_test = {rmse_test}, mae_test = {mae_test}, r2_test = {r2_test}")
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("min_samples_leaf", min_samples_leaf)
+        mlflow.log_param("min_samples_split", min_samples_split)
         mlflow.log_metric("rmse_train", rmse_train)
         mlflow.log_metric("mae_train", mae_train)
         mlflow.log_metric("r2_train", r2_train)
@@ -77,11 +94,11 @@ if __name__ == '__main__':
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
-                model_en, "model", registered_model_name="ElasticnetWineModel"
+                model_dtr, "model", registered_model_name="ElasticnetWineModel"
             )
         else:
-            mlflow.sklearn.log_model(model_en, "model")
+            mlflow.sklearn.log_model(model_dtr, "model")
 
 
-    # with open('./artifacts/bhp_model_en.pickle', 'wb') as f:
-    #     pickle.dump(model_en, f)
+    # with open('./artifacts/bhp_model_dtr.pickle', 'wb') as f:
+    #     pickle.dump(model_dtr, f)
